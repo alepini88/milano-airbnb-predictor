5ed7a3a6ecc81637b19d9c6be620463518dd4d58
alepini88 <51296824+alepini88@users.noreply.github.com>
2026-05-05 15:30:21 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import numpy as np
 # 1. Carichiamo il "cervello" (modello) e le colonne
 model = joblib.load('modello_airbnb.pkl')
 features = joblib.load('features.pkl')
+dict_quartieri = joblib.load('dist_quartieri.pkl')
 
 st.title("🏠 Airbnb Milano: Suggeritore di Prezzo")
 st.write("Inserisci i dettagli del tuo alloggio per sapere a quanto affittarlo.")
@@ -14,13 +15,15 @@ st.write("Inserisci i dettagli del tuo alloggio per sapere a quanto affittarlo."
 col1, col2 = st.columns(2)
 
 with col1:
-    acc = st.slider("Persone ospitate", 1, 10, 2)
+    acc = st.slider("Persone ospitate", 1, 6, 2)
     rooms = st.number_input("Numero stanze", 1, 5, 1)
     dist = st.number_input("Distanza dal Duomo (km)", 0.0, 10.0, 2.0)
 
 with col2:
-    bath_per_p = st.number_input("Rapporto Bagni/Persone", 0.1, 1.0, 0.5)
+    
     reviews = st.number_input("Numero di recensioni", 0, 500, 10)
+    quartiere_scelto = st.selectbox("In quale quartiere si trova l'alloggio?", list(dict_quartieri.keys()))
+    valore_quartiere = dict_quartieri[quartiere_scelto]
     # Aggiungi qui gli altri input (latitudine, longitudine, ecc.)
 
 # 3. Il tasto magico
@@ -31,6 +34,7 @@ if st.button("Calcola Prezzo Ottimale"):
     dati_utente['accommodates'] = acc
     dati_utente['distance_from_center'] = dist
     dati_utente['number_of_reviews'] = reviews
+    dati_utente['neighbourhood_price_median'] = valore_quartiere 
     # ... compila tutte le altre variabili ...
 
     df_input = pd.DataFrame([dati_utente])
